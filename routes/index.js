8dec261229ca9475ead09a7f978a9747fe20f37f
Nuno Silva <gamesflash94@gmail.com>
2017-10-16 18:29:55 +0100

    Added Firebase SDK

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require('../firebase/serviceAccountKey.json');
+
+var firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tracker-53924.firebaseio.com"
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: firebaseApp.name });
 });
 
 module.exports = router;
