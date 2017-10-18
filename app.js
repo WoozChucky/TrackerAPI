e993051eae7f45f3acbf61bf4a2c3ce38ad80c70
Nuno Silva <gamesflash94@gmail.com>
2017-10-19 00:24:01 +0100

    Added SASS middleware to dependencies

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +25,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  sassMiddleware({
+    src: __dirname + '/sass', 
+    dest: __dirname + '/public',
+    debug: true,       
+  })
+);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,40 @@ var firebaseApp = admin.initializeApp({
   databaseURL: "https://tracker-53924.firebaseio.com"
 });
 
+var payload = {
 
+  notification : {
+    title: "Example Notification",
+    body: "Notification body"
+  },
+
+  data : {
+    stock: "GOOG",
+    open: "829.62",
+    close: "635.67"
+  }
+
+};
+
+var token = 'c7CI4gHs2MQ:APA91bFDw2_Ab77xR3IBhQBh4nIKSko8Q62-vaDRHb6-vfIjpOz3JdH4GAppv0qycDwgiAwKksg1L1BS8Ll4jA6XSWDyaj0JqiZDNA1WalgKXdnRB0trQ3_hxlmSfyGc5KNcriSGv3WC';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: firebaseApp.name });
+
+  /*
+  firebaseApp.messaging().sendToDevice(token, payload)
+  .then(response => {
+    console.log('Successfully sent message to ', + response.successCount);
+  })
+  .catch(error => {
+    console.log("Error sending message:", error);
+  }); */
+
+  res.render('index', 
+    { title: firebaseApp.name,
+      message: "" 
+    }
+  );
 });
 
 module.exports = router;
