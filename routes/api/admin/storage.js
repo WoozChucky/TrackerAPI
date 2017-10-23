1eb2e7d7450ef5c3101c17e722dd93a2ab5d4d1e
Nuno Silva <gamesflash94@gmail.com>
2017-10-23 11:34:06 +0100

    Added upload feature in storage controller

--- routes/api/admin/storage.js
+++ routes/api/admin/storage.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer');
+var upload = multer({ dest: './uploads' });
 
 var firebaseApp = require('../../../firebase/firebase_app.js');
 
@@ -8,12 +10,38 @@ var firebaseApp = require('../../../firebase/firebase_app.js');
  */
 router.get('/', function(req, res, next) {
     
-    var rootReference = firebaseApp.storage().ref();
+    
+
+    //
     
     //var uploadTask = rootReference.child('temp/' + filename).put(file, metadata);
     
     
 
+});
+
+router.post('/upload', upload.single('fileinput'), (req, res, next) => {
+    console.log('Entered /upload !');
+    console.log(req.file);
+
+    var file = req.file;
+
+    // Create the file metadata
+    var metadata = {
+        contentType: 'image/jpeg'
+    };
+
+    var rootReference = firebaseApp.storage().bucket('temp');
+
+    rootReference.upload(file.path, {destination: file.filename })
+        .then(storageFile => {
+            console.log(storageFile);
+        })
+        .catch(reason => {
+            console.log(reason);
+        });
+
+    res.render('admin/storage');
 });
 
 
