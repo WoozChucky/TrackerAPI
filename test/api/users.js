1542ed894cf7c2f8a905198d1ec96f621bb91948
Nuno Silva <gamesflash94@gmail.com>
2017-10-26 15:52:09 +0100

    Updated test to match new authenticate route

--- test/api/users.js
+++ test/api/users.js
@@ -7,13 +7,13 @@ let should = require('should');
 chai.use(chaiHttp);
 
  /**
- * Test the /GET api/user/login
+ * Test the /GET api/user/authenticate
  */
 
-describe('/GET api/user/login', () => {
+describe('/GET api/user/authenticate', () => {
     it('should get htpp 400 from authentication process', done => {
         chai.request(server)
-            .post('/api/user/login')
+            .post('/api/user/authenticate')
             .send({uid: 'invalid_token'})
             .end((err, res) => {
                 should.equal(res.status, 400);
